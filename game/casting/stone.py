24ce8fa7dc5a875e83b7c94c1f7f005e8df9c846
jjsn10 <san14038@byui.edu>
2022-07-09 20:09:10 -0600

    folder that contains game classes

---
+++ game/casting/stone.py
@@ -0,0 +1,15 @@
+from game.casting.actor import Actor
+from game.shared.point import Point
+import random
+
+class Stone (Actor):
+    def __init__(self):
+        super().__init__()
+
+    def fall(self,max_y):
+        x = self._position.get_x()
+        y = (self._position.get_y()+5) % max_y
+        self._position = Point(x,y)
+    def changingX(self):
+        x = random.randint(1,59)*15
+        self._position = Point(x,self._position.get_y())
